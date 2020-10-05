4f88bee8db79ecb55bf6311158358cd9315c25d5
Austin Gordon <67164152+austincgordon@users.noreply.github.com>
2020-10-05 12:38:32 -0500

    Final Commit
    Fixed game reset functionality, project now works as expected.

--- app.js
+++ app.js
@@ -18,10 +18,13 @@ let phrases = [
 startGame.addEventListener('click', () => {
   // Resets the hearts
   let heart = document.getElementsByClassName('tries');
+  missed = 0;
   for (let i = 0; i < heart.length; i++) {
     heart[i].style.display = 'inline-block';
   }
 
+  let phraseArray = getRandomPhraseAsArray(phrases);
+  addPhraseToDisplay(phraseArray);
   startScreen.style.display = 'none';
   startScreen.classList.remove('win', 'lose');
 
@@ -39,13 +42,12 @@ function getRandomPhraseAsArray(arr) {
   return arrayOfStrings;
 }
 
-let phraseArray = getRandomPhraseAsArray(phrases);
-
 // Adds Random Phrase to the Screen
 function addPhraseToDisplay(arr) {
+  let ul = document.getElementsByTagName('ul')[0];
+  ul.innerHTML = '';
   for (let i = 0; i < arr.length; i++) {
     let listItem = document.createElement('li');
-    let ul = document.getElementsByTagName('ul')[0];
     if (arr[i] !== ' ') {
       listItem.className = 'letter';
     } else {
@@ -72,8 +74,6 @@ function checkLetter(buttons) {
   return match;
 }
 
-addPhraseToDisplay(phraseArray);
-
 // Checks to see if the player won or lost the game
 function checkWin() {
   const letterLi = document.getElementsByClassName('letter');
@@ -99,18 +99,19 @@ function checkWin() {
 
 // Listens for correct or incorrect button clicks on the on-screen keyboard
 qwerty.addEventListener('click', (e) => {
+  let letterResult = checkLetter(e.target);
+
   // Checks to see if a button was pressed
   if (e.target.tagName === 'BUTTON') {
     e.target.className = 'chosen';
     e.target.setAttribute('disabled', true);
-  }
 
-  let letterResult = checkLetter(e.target);
+    if (letterResult === null) {
+      let heart = document.getElementsByClassName('tries');
 
-  if (letterResult === null) {
-    let heart = document.getElementsByClassName('tries');
-    heart[missed].style.display = 'none';
-    missed++;
+      heart[missed].style.display = 'none';
+      missed++;
+    }
   }
 
   checkWin();
